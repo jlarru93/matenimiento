eb1f1d57436dcfee270f05322ba60d5a26f04402
Filipe <filipe.chaves@gmail.com>
2019-07-17 14:36:21 -0300

    fix credentials mongeez

--- src/main/java/pe/com/banbif/correo/eletronico/service/ApplicationMail.java
+++ src/main/java/pe/com/banbif/correo/eletronico/service/ApplicationMail.java
@@ -1,13 +1,15 @@
 package pe.com.banbif.correo.eletronico.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableScheduling
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,  HibernateJpaAutoConfiguration.class})
 public class ApplicationMail {
 
     public static void main(String... args) {
